ace75f92e5b86f118f059f8fe1143d7d42528c66
konstantinosanagn <ka3037@columbia.edu>
2025-10-29 21:35:18 -0400

    Set legacy OpenSSL provider in webpack production config

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,8 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
+// Set legacy OpenSSL provider for Node.js 17+
+process.env.NODE_OPTIONS = '--legacy-openssl-provider'
+
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
