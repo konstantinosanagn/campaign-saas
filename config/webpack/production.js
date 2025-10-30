a581c2ebab8ce63501049a57a8a5887c39d65943
konstantinosanagn <ka3037@columbia.edu>
2025-10-29 21:46:25 -0400

    Pin Node engine to 16.x for webpacker compatibility

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,11 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-// Configure webpack to use legacy OpenSSL provider
-const config = environment.toWebpackConfig()
-
-// Override the hash function to use a supported algorithm for Webpack 4
-config.output = config.output || {}
-config.output.hashFunction = 'sha256'
-
-module.exports = config
+module.exports = environment.toWebpackConfig()
