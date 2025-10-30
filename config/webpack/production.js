994a2bee61f30031b010d44ad8361b1b570a4456
konstantinosanagn <ka3037@columbia.edu>
2025-10-29 21:40:05 -0400

    Fix webpack hashFunction configuration for Webpack 4

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -5,8 +5,8 @@ const environment = require('./environment')
 // Configure webpack to use legacy OpenSSL provider
 const config = environment.toWebpackConfig()
 
-// Override the hash function to use legacy algorithm
-config.optimization = config.optimization || {}
-config.optimization.hashFunction = 'xxhash64'
+// Override the hash function to use legacy algorithm for Webpack 4
+config.output = config.output || {}
+config.output.hashFunction = 'xxhash64'
 
 module.exports = config
