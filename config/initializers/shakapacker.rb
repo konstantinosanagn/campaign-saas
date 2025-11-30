882271ce99c587a3232b97e923ca51288fa12475
konstantinosanagn <ka3037@columbia.edu>
2025-11-29 23:33:49 -0500

    Fix Heroku build: remove invalid Shakapacker monkey-patch
    Remove monkey-patch that referenced non-existent method, rely on
    PACKAGE_JSON_FALLBACK_MANAGER env var set in application.rb instead.

--- config/initializers/shakapacker.rb
+++ config/initializers/shakapacker.rb
@@ -1,14 +1,3 @@
-# Backup: Monkey-patch Shakapacker to skip packageManager check if yarn.lock exists
-# This is a safety net in case the PACKAGE_JSON_FALLBACK_MANAGER env var isn't picked up
-if File.exist?(Rails.root.join('yarn.lock')) && defined?(Shakapacker::Utils::Manager)
-  Shakapacker::Utils::Manager.class_eval do
-    alias_method :original_error_unless_package_manager_is_obvious!, :error_unless_package_manager_is_obvious!
-    
-    def error_unless_package_manager_is_obvious!
-      # Skip the packageManager check if yarn.lock exists
-      # This allows Heroku builds to work without corepack
-      return if File.exist?(Rails.root.join('yarn.lock'))
-      original_error_unless_package_manager_is_obvious!
-    end
-  end
-end
+# PACKAGE_JSON_FALLBACK_MANAGER is set in config/application.rb before Bundler.require
+# This ensures Shakapacker uses yarn when packageManager is not set in package.json
+# No monkey-patch needed - the env var should be sufficient
